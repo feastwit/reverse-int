3afb2ed219130bf3baead9db18ab28b8e66a3076
Krystsina Pirumian <kytia@yandex.ru>
2022-08-10 20:40:01 +0300

    init: create project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if(n > 0){
+        return Number(n.toString().split("").reverse().join(""))
+      }else if(n < 0){
+        let b = n*(-1)
+        return Number(b.toString().split("").reverse().join(""))
+      }
 }
